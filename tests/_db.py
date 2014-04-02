9e8be7aea77378e447374918d00831893aa933b7
yelite <yelite958@gmail.com>
2014-04-02 13:42:54 +0800

    test db path changed

--- tests/_db.py
+++ tests/_db.py
@@ -6,7 +6,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 MAIN_DIC = os.path.split(os.path.realpath(__file__))[0]
-DB_FILE = os.path.join(MAIN_DIC, '_data.db')
+DB_FILE = os.path.join(MAIN_DIC, 'data/data.db')
 
 engine = create_engine('sqlite:///{}'.format(DB_FILE))
 Session = sessionmaker(bind=engine)
