5b67136f93dd3e5bb71b524310fd099a37134a9f
yelite <yelite958@gmail.com>
2014-04-05 14:56:41 +0800

    Add BasePredictor to models

---
+++ models/base.py
@@ -0,0 +1,36 @@
+#coding=utf-8
+
+
+from data import Data
+from static import StaticData
+from sqlalchemy import desc
+
+
+class BasePredictor(object):
+    def __init__(self, ext_session, static_data=None, test=False):
+        self.ext_session = ext_session
+        self.test = test
+
+        self.data_query = self.ext_session.query(Data)
+
+        self.end_date = ext_session.query(Data.time).order_by(desc(Data.time)).first()[0]
+        self.start_date = ext_session.query(Data.time).order_by(Data.time).first()[0]
+        self.length = (self.end_date - self.start_date).days
+
+        self.count = 0
+        self.all = 0
+
+        if static_data:
+            self.static_data = static_data
+        else:
+            self.static_data = StaticData(test=test)
+        self.user_brand = self.static_data.user_brand
+        self.brand_user = self.static_data.brand_user
+
+    def begin(self):
+        self.count = 0
+
+    def done(self):
+        self.count += 1
+        if self.count % 128 == 0:
+            print self.count / self.all
